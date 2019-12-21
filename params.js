d29d50c3268acae173a18e07e4b5199063839479
Nicolas CARREZ <nicolas.carrez@clustree.com>
2019-12-21 16:03:42 +0100

    user Perm redesign

--- params.js
+++ params.js
@@ -2,6 +2,6 @@ const { ParamsDef } = Msa.require("params")
 const { SheetPerm } = require("./perm")
 
 const sheetParamsDef = new ParamsDef()
-sheetParamsDef.add("perm", SheetPerm.newPermParamDef(SheetPerm.READ))
+sheetParamsDef.add("perm", SheetPerm.newPermParamDef({ group:"all", value:SheetPerm.READ }))
  
 module.exports = { sheetParamsDef }
