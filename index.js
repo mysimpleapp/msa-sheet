a655e32d768113fef40354e2bdfcd43313ba92d5
Nicolas CARREZ <ncarrez@csod.com>
2020-05-10 15:18:16 +0200

    Use installMsaModule

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mod = require("./module")
 module.exports = {
+    installMsaModule: async itf => {
+        await require("./install")(itf)
+    },
     startMsaModule: () => new mod.MsaSheet(""),
     ...mod
 }
